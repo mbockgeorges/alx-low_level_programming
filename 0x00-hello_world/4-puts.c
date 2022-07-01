b21611d986ab317f27e852f709ab4ff6689b91b9
mbockgeorges <mbochristian89@gmail.com>
2022-06-30 21:00:22 -0700

    my commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
-#!/bin/bash
 #include <stdio.h>
-int main()
+
+int main(void)
 {
-    char str ="/"Programming is like building a multilingual puzzle" 
-    puts (str);
-    return 0 ;    
+	puts("\"Programming is like building a multilingual puzzle");
+	 return 0 ;    
 }

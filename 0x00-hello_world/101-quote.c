e26e0ff260032c4a3015ca4040f9a2e106bfa7b1
mbockgeorges <mbochristian89@gmail.com>
2022-07-01 14:40:59 -0700

    my commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *the use the function scanf
+ *Return: Always 1 (Success)
+ *
+ */
+
+int main(void)
+{
+	scanf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}

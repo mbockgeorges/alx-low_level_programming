fcee7cc46327173eca9ab3f8e08e122bc3871458
mbockgeorges <mbochristian89@gmail.com>
2022-07-01 05:10:32 -0700

    my commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,8 +16,8 @@ int main(void)
 	
 	printf("size of a char: %lu byte(s)\n", sizeof(c));
 	printf("size of a float: %lu byte(s)\n", sizeof(f));
-	printf("size of a long long int: %lu byte(s)", sizeof(l));
+	printf("size of a long long int: %lu byte(s)\n", sizeof(l));
 	printf("size of a long int: %lu byte(s)\n", sizeof(I));
-	printf("size of an int: %lu byte(s)", sizeof(i));
+	printf("size of an int: %lu byte(s)\n", sizeof(i));
 	return (0);
 }

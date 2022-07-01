89dc15b877593587ac6b54e6ef281bc005906a23
mbockgeorges <mbochristian89@gmail.com>
2022-07-01 01:21:16 -0700

    my commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
-#include <stdio.h> 
+#include <stdio.h>
 
 
-/** 
- *main - Entry point 
- *the use funtion puts 
- *Return: Always 0(Success) 
- */ 
-int main(void) 
-{ 
-	puts("\"Programming is like building a multilingual puzzle"); 
-	return (0); 
-} 
+/**
+ *main - Entry point
+ *the use funtion puts
+ *Return: Always 0(Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

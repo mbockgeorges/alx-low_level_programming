b0f42b5bd7f031d9ee6d9edf054f7c13fa71603d
mbockgeorges <mbochristian89@gmail.com>
2022-07-04 18:00:36 -0700

    my commit

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,7 +3,7 @@
 /**
  *main -Prints all singledigit number of base 10 starting from 0
  *the use function print
- *Retur: Always 0 (Success)
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -11,7 +11,7 @@ int main(void)
 	int n;
 
 	for (n = 0; n < 10; n++)
-		printf("%d \n", n);
+		printf("%d", n);
 
 	return (0);
 

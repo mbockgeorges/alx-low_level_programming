e937f1ea6b72afa7955ef89054b0cb62080d778c
mbockgeorges <mbochristian89@gmail.com>
2022-07-06 01:27:06 -0700

    my commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *main -print alphabet
+ *the use function _putchar
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print_alphabet();
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int _putchar(char c)
+{
+	printf("%c\n", c);
+
+	return (0);
+}

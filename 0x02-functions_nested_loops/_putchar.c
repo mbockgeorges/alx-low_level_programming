2053b2528691663c4ca6cc0fba9fa809d8353239
mbockgeorges <mbochristian89@gmail.com>
2022-07-06 02:51:11 -0700

    my commit

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
+#include "main.h"
 
 int _putchar(char c)
 {
 	printf("%c\n", c);
 
 	return (0);
+}
+
+void print_alphabet(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+		_putchar(l);
+
+	_putchar('\n');
 }

7a056bc36119536f2e475e4508bdf8af97df4f2d
mbockgeorges <mbochristian89@gmail.com>
2022-07-07 14:01:31 -0700

    my commit

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ * Description:  finds and prints the largest prime factor of 612852475143
+ * Return:0
+ */
+
+int main(void)
+{
+
+	unsigned long number = 612852475143;
+	unsigned long prime number = 1;
+	unsigned long i = 2;
+
+	while (i <= number)
+	{
+		if (number % i == 0)
+		{
+			if (i > prime_number)
+				prime_number = 1;
+			number = number / i;
+		}
+		else
+			i++;
+	}
+
+	if (prime_number >= 2)
+		printf("%lu\n", prime_number);
+
+	return (0);
+
+}
---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_number - prints
+ * @n: integer
+ * Description: prints an integer
+ * Return: nothing
+ */
+void print_number(int n)
+{
+
+	int d = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		d = -n;
+	}
+
+	if (d > 9)
+	{
+		print_number(d / 10);
+		_putchar((d % 10) + '0');
+	}
+	else
+		_putchar(d + '0');
+}

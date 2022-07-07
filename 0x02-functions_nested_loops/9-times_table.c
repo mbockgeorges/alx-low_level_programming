b519673d34c85fcc112bcc543eb050ce23f6958a
mbockgeorges <mbochristian89@gmail.com>
2022-07-06 17:32:42 -0700

    my commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,30 +9,30 @@ void times_table(void)
 {
 	int x, y, z;
 
-	for (x = 0; x <= 9; x++)
-	{
-		for (y = 0; y <= 9; y++)
+		for (x = 0; x <= 9; x++)
 		{
-			z = x * y;
-			if (z < 100 && y != 0)
-				_putchar(' ');
-			else if (y != 0)
-				_putchar((z / 100) + '0');
-
-			if (z > 9)
-				_putchar(((z / 10) % 10) + '0');
-			else if (k != 0)
-				_putchar(' ');
-
-			_putchar((z % 10) + '0');
-
-			if (y == 9)
-				_putchar('\n');
-
-			else
+			for (y = 0; y <= 9; y++)
 			{
-				_putchar(',');
+				z = x * y;
+				if (z < 100 && y != 0)
+					_putchar(' ');
+				else if (y != 0)
+					_putchar((z / 100) + '0');
+
+				if (z > 9)
+					_putchar(((z / 10) % 10) + '0');
+				else if (k != 0)
+					_putchar(' ');
+
+				_putchar((z % 10) + '0');
+
+				if (y == 9)
+					_putchar('\n');
+
+				else
+				{
+					_putchar(',');
+				}
 			}
 		}
-	}
 }

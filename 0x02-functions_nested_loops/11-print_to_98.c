bbfb8116dde00087ea90df2ce4c23863d6cbd665
mbockgeorges <mbochristian89@gmail.com>
2022-07-06 19:00:46 -0700

    my commit

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ *print_to_98 - Prints
+ *@n : integer
+ *Description: determine the last digit
+ *Return: last digit of @n
+ */
+void print_to_98(int n)
+{
+	int j;
+
+	if (n < 98)
+	{
+		for (j = n; j <= 9; j++)
+		{
+			printf("%d", j);
+			if (j != 98)
+				printf(",");
+		}
+	}
+
+	else
+	{
+		for (j = n; j >= 9; j++)
+		{
+			printf("%d", j);
+			if (j != 98)
+				printf(",");
+		}
+	}
+
+	printf("\n");
+
+}

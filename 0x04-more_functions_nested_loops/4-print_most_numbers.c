98457d74a973433e472eb301fb05f1db0de2687d
mbockgeorges <mbochristian89@gmail.com>
2022-07-07 10:42:26 -0700

    my commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,5 +12,6 @@ void print_most_numbers(void)
 	for (d = '0'; d <= '9'; d++)
 		if (d != '2' && d != '4')
 			_putchar(d);
+
 	_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_MORE
 #define MAIN_MORE
+int _putchar(char c);
 
 int _isupper(int c);
 int _isdigit(int c);

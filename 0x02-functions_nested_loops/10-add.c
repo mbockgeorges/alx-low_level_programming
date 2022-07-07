c37a69ccf9ee8d5d0679bfaed49862286c0b3360
mbockgeorges <mbochristian89@gmail.com>
2022-07-06 18:34:25 -0700

    sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds
+ * @x : first number
+ * @y : second number
+ * Description: determine the sum of to @x and @y
+ * Return: @x + @y
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}

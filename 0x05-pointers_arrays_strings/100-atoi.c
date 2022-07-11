f5ebf89dac92b6fca4e1d6c0f28611af42372b8b
mbockgeorges <mbochristian89@gmail.com>
2022-07-11 11:47:53 -0700

    my commit

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdio.h>
+#include <limits.h>
+
+/**
+ *_atoi - function
+ *@s:char pointer
+ *Description:a function that convert a string to an integer.
+ *Return:the integer value
+ */
+int _atoi(char *s)
+{
+	int n = 0;
+	int i = 0;
+	int sign = 1;
+	int is_n = (-1);
+
+	if (s == NULL)
+		return (n);
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			sign *= (-1);
+		else if ('0' <= s[i] && s[i] <= '9')
+		{
+			if (is_n == 0)
+				break;
+			else if (is_n == (-1))
+				is_n = 1;
+
+			if ((n * 10) >= INT_MAX - (s[i] - '0'))
+				n = INT_MAX;
+			else
+				n = (n * 10) + (s[i] - '0');
+		}
+		else if (is_n == 1)
+			is_n = 0;
+		i++
+	}
+
+	if (n == INT_MAX && sign == (-1))
+		n = (-n) - 1;
+	else
+		n *= sign;
+
+	return (n);
+}

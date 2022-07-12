49fbe8d1bfc14c6824b16d437c94ca352cf97509
mbockgeorges <mbochristian89@gmail.com>
2022-07-11 21:12:12 -0700

    my commit

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,32 +1,47 @@
 #include "main.h"
-#include <stdio.h>
+#include <stdio>i
 
 /**
  *_atoi - function
  *@s:char pointer
- *Description:a function that convert a string to an integer
- *Return: integer
+ *Description:a function that convert a string to an integer.
+ *Return:the integer value
  */
 int _atoi(char *s)
 {
-	int sig = 1;
+	int number = 0;
 	int i = 0;
-	unsigned int res = 0;
+	int signe = 1;
+	int is_number = -1;
 
-	while (!(s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
+	if (s == NULL)
+		return (number);
+
+	while (s[i] != '\0')
 	{
 		if (s[i] == '-')
-			sig *= -1;
+			signe *= -1;
+		else if ('0' <= s[i] && s[i] <= '9')
+		{
+			if (is_number == 0)
+				break;
+			else if (is_number == -1)
+				is_number = 1;
+
+			if ((number * 10) >= INT_MAX - (s[i] - '0'))
+				number = INT_MAX;
+			else
+				number = (number * 10) + (s[i] - '0');
+		}
+		else if (is_number == 1)
+			is_number = 0;
 		i++;
 	}
 
-	while (s[i] <= '9' && (s[i] >= '0' && s[i] != '\0'))
-	{
-		res = (res * 10) + (s[i] - '0')
-		i++;
-	}
+	if (number == INT_MAX && signe == -1)
+		number = (-number) - 1;
+	else
+		number *= signe;
 
-	res *= sig;
-
-	return (res);
+	return (number);
 }

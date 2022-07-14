1fe5bc617ce38f2731212ec79efdef8d206a676a
mbockgeorges <mbochristian89@gmail.com>
2022-07-14 11:33:10 -0700

    my commit

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <limits.h>
+
+/**
+ *print_number - function
+ *@n:integer
+ *Description: a function that prints an integer.
+ *Return:nothing
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar(' ');
+		print_number((n == INT_MIN) ? -(n / 10) : -n);
+		if (n == INT_MIN)
+			_putchar((-(n - ((n / 10) * 10))) + '0');
+	}
+	else if (0 <= n && n <= 9)
+	{
+		_putchar(n + '0');
+	}
+	else
+	{
+		print_number(n / 10);
+		_putchar((n % 10) + '0');
+	}
+}

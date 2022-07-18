f19c53a1b3fe985fd87f4bfd232279d31bee0e4f
mbockgeorges <mbochristian89@gmail.com>
2022-07-18 11:56:27 -0700

    my commit

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *_memset - function
+ *@s: char pointer
+ *@b: character
+ *@n: unsigned integer
+ *Description:a function that fills memory with a constant byte
+ *Return: result
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	if (s == 0)
+		return (s);
+
+	for (i = 0; i < n; i++)
+		*(s + i) = b;
+
+	return (s);
+}

af25875814c347fb3f186b45bff0f24388f62537
mbockgeorges <mbochristian89@gmail.com>
2022-07-18 16:08:11 -0700

    my commit

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *print_diagsums - function
+ *@a:integer pointer
+ *@size:integer
+ *Description:a function that prints the sum of the two
+ *diagonals of a square
+ *Return:nothing
+ */
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int sum1 = 0;
+	int sum2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 += a[i * size + 1];
+		sum2 += a[i * size + (size - i - 1)];
+	}
+
+	printf("%d, %d\n", sum1, sum2);
+}

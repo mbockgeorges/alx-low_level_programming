cfebd031c9eeb440b7f7d79150019927ef99b052
mbockgeorges <mbochristian89@gmail.com>
2022-07-20 13:12:43 -0700

    my commit

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ *_sqrt_recursion - function
+ *@n: integer
+ *Description:a function that returns the natural square root of a number.
+ *Return:result
+ */
+int _sqrt_recursion(int n)
+{
+	if (a * a > A)
+		return (-1);
+	if (a * a == A)
+		return (a);
+	return (real_sqrt(A, a + 1));
+}
+
+/**
+ *_sqrt_recursion - sqrt
+ *@n:integer
+ *Description:determine natural square root
+ *Return:result
+ */
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+		return (0);
+
+	return (real_sqrt(n, 1));
+}

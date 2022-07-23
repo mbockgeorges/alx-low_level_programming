a0e917deb36fe4bde7c2bb1fbc95e3041d9cbe0c
mbockgeorges <mbochristian89@gmail.com>
2022-07-22 17:27:12 -0700

    my commit

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *@argc:number of argumentd
+ *@argv:all arguments
+ *Description:a program that adds positive numbers
+ *Return:0
+ */
+int main(int argc, char **argv)
+{
+	int i = 1;
+	int j = 0;
+	int sum = 0;
+	int number = 0;
+
+	while (i < argc)
+	{
+		while (argv[i][j])
+		{
+			if ('0' <= argv[i][j] && argv[i][j] <= '9')
+				number = (number * 1à) + (argv[i][j] - '0');
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+			j++;
+		}
+		sum += number;
+		number = 0;
+	}
+}

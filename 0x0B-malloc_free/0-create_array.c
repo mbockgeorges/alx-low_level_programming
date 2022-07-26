2c526450a3da07124f1cf7e3d1f5f289291a797e
mbockgeorges <mbochristian89@gmail.com>
2022-07-26 16:52:43 -0700

    my commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,7 +16,7 @@ char *create_array(unsigned int size, char c)
 	if (size > 0)
 	{
 		unsigned int i = 0;
-		char *array = (char *)malloc(sizeof(char) * siez);
+		char *array = (char *)malloc(sizeof(char) * size);
 
 		if (array != 0)
 		{

a30441d39b5186cbffbc6f182bfb3c09b88855d8
mbockgeorges <mbochristian89@gmail.com>
2022-07-28 15:40:03 -0700

    my commit

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib>
+
+/**
+ *_calloc - new allocation
+ *@nmemb:elements of size bytes
+ *@size: number of bytes
+ *Description:allocates memory for an array using malloc
+ *Return:memory is set to zero
+ */
+void *_calloc(usigned int nmemb, unsigned int size)
+{
+	void *memory = 0;
+	unsigned int i = 0;
+
+	if (nmemb <= 0 || size <= 0)
+		return (0);
+	memory = malloc(nmemb * size);
+
+	if (!memory)
+		return (0);
+	for (i = 0; i < (nmemb * size); i++)
+		*((char *)memory + i) = 0;
+	return (memory);
+}

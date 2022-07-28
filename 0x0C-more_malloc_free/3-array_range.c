27ab2745fe07f17038b5870e82ffc17d1a588886
mbockgeorges <mbochristian89@gmail.com>
2022-07-28 16:19:39 -0700

    my commit

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *array_array -array
+ *@min:min value
+ *@max:max value
+ *Description:the array created should contain all the values
+ *from min (included) to max (included)
+ *Return:pointer
+ */
+int *array_range(int min, int max)
+{
+	int *table = 0;
+	int i = 0;
+
+	if (min > max)
+		return (0);
+
+	table = (int *)malloc(sizeof(int) * (max - min + 1));
+
+	if (!table)
+		return (0);
+
+	for (i = 0; i < (max - min + 1); i++)
+		table[i] = min + i;
+
+	return (table);
+}

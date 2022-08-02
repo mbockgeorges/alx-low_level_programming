0f8a5d4118c5a91a0f89b5c66b5c6e7ac481b403
mbockgeorges <mbochristian89@gmail.com>
2022-08-02 16:08:03 -0700

    my commit

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -4,9 +4,9 @@
  * init_dog -initialize a struct
  *@d:pointer to structure
  *@name:pointer to the name
- * @age:age of dog
- * @owner:pointer to owner
- * Return:void
+ *@age:age of dog
+ *@owner:pointer to owner
+ *Return:void
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {

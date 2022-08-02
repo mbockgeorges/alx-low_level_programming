c44d4beef85afbcecc709c7b69367a4d08dd9ecf
mbockgeorges <mbochristian89@gmail.com>
2022-08-02 16:04:39 -0700

    my commit

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/**
+ * init_dog -initialize a struct
+ *@d:pointer to structure
+ *@name:pointer to the name
+ * @age:age of dog
+ * @owner:pointer to owner
+ * Return:void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}

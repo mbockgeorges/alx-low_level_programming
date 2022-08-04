2ebd237337537e6cb30530d7790c477d150864d7
mbockgeorges <mbochristian89@gmail.com>
2022-08-04 16:54:43 -0700

    my commit

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+
+/**
+ *print_numbers - that prints numbers
+ *@separator:separator between two numbers
+ *@n:number of parametres
+ *Return:number to print
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	int num;
+	va_list pa;
+
+	va_start(pa, n);
+	for (i = 0; i < n; i++)
+	{
+		num = va_arg(pa, int);
+		printf("%d", num);
+		if (i < n - 1 && separator)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(pa);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,4 @@
+T0)Write a function that returns the sum of all its parameters.
+T1)Write a function that prints numbers, followed by a new line.
+T2)Write a function that prints strings, followed by a new line.
+T3)Write a function that prints anything.

c8d7e6af1ce5c4fd347068fe21bfdea731cc1296
mbockgeorges <mbochristian89@gmail.com>
2022-08-03 19:03:22 -0700

    my commit

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+
+/**
+ *array_iterator - function
+ *parameter on each element of an array
+ *@array:array
+ *@size:size to array
+ *@action:is a pointer to the function
+ *Return:void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int));
+{
+	size_t i;
+
+	if (array && action)
+	{
+		i = 0;
+		while (i < size)
+		{
+			action(arra[i]);
+			i++;
+		}
+	}
+}

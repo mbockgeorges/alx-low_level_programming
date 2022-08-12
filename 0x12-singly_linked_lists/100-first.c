bcadc8ed65c5bb173a097f9968baf43785bc46e9
mbockgeorges <mbochristian89@gmail.com>
2022-08-11 17:44:20 -0700

    my commit

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ *bmain - function
+ *
+ *
+ *...
+ */
+void __attribute__((constructor)) bmain()
+{
+	printf("You're beat§ and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}

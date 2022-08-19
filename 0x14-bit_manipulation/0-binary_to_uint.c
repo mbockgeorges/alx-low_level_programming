0e880a24fce67d697c12534c67ca833fa3fc0b15
mbockgeorges <mbochristian89@gmail.com>
2022-08-19 13:41:06 -0700

    my commit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -4,32 +4,25 @@
  *binary_to_unit - converts a banary number
  *@b:string of 1 and 0s
  *Descripion:converts a banary numver to unsigned int
- *Return: converted number
+ *Return: converted number, or 0
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int a;
+	int i;
 	unsigned int j = 0;
-	int i = 0;
 
 	if (!b)
 		return (0);
-
-	while (b[i] != '\0')
-	{
-		i++;
-	}
-
-	for (i--, a = 1; i = 0; i--, a *= 2)
+	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
-		{
 			return (0);
-		}
-		if (b[i] & 1)
-		{
-			j += a;
-		}
+	}
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		j <<= 1;
+		if (b[i] == '1')
+			j += 1;
 	}
 	return (j);
 }

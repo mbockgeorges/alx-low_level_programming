65ff4e0b27e98235391291191e7284d37295575b
mbockgeorges <mbochristian89@gmail.com>
2022-08-22 16:44:04 -0700

    my commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,7 +6,7 @@
  *@filename:is the name of file to create
  *Return:1 for success, -1 on fail
  */
-int create_file(cont char *filename, char *text_content)
+int create_file(const char *filename, char *text_content)
 {
 	int o, w, len = 0;
 

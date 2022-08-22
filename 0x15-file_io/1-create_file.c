66bde96a2c820bd8433e6ef6d6b580db16c1a87e
mbockgeorges <mbochristian89@gmail.com>
2022-08-22 15:58:57 -0700

    my commit

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *create_file -creates an array of chars
+ *@text_content:is a NULL termited string to write
+ *@filename:is the name of file to create
+ *Return:1 for success, -1 on fail
+ */
+int create_file(cont char *filename, char *text_content)
+{
+	int o, w, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+
+	return (1);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,4 @@
+T0)Write a function that reads a text file and prints it to the POSIX standard output.
+T1)Create a function that creates a file.
+T2)Write a function that appends text at the end of a file.
+T3)Write a program that copies the content of a file to another file.

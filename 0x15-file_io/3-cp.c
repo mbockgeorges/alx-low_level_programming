546a4bddbd985c714dc4bccaae5af89e45914be7
mbockgeorges <mbochristian89@gmail.com>
2022-08-22 16:53:06 -0700

    my commit

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,7 +11,7 @@ void error_file(int file_from, int file_to, char *argv[])
 {
 	if (file_from == -1)
 	{
-		dprintf(STDERR, "Error: can't read from file %s\n", argv[1])
+		dprintf(STDERR_FILENO, "Error: can't read from file %s\n", argv[1]);
 			exit(98);
 	}
 	if (file_to == -1)
